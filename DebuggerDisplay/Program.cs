b2801c1cb6b0e8372956656e172e8bae9ddb1441
Mallikarjuna Kalkere <mkalkere@gmail.com>
2017-10-09 21:06:09 -0400

    Used DebuggerDisplay attribute

--- DebuggerDisplay/Program.cs
+++ DebuggerDisplay/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,39 @@ namespace DebuggerDisplay
     {
         static void Main(string[] args)
         {
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer("James","Butt"),
+                new Customer("Josephine","Darakjy"),
+                new Customer("Art","Venere"),
+                new Customer("Lenna","Paprocki"),
+                new Customer("Donette","Foller"),
+                new Customer("Simona","Morasca")
+            };
         }
+    }
+    ////Method ::: 2
+    ////DebuggerDisplay attribute
+    [DebuggerDisplay("Name: {FullName,nq}")]
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName { get => $"{FirstName} {LastName}"; }
+
+        public Customer(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        ////Method ::: 1
+        ////Override ToString()
+
+        //public override string ToString()
+        //{
+        //    return FullName;
+        //}
     }
 }
